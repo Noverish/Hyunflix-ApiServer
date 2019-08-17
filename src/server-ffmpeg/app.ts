247b8080e46a8b4dfc2f26397e4556515f564b6b
Hyunsub Kim <embrapers263@gmail.com>
2019-08-17 14:23:20 +0900

    Changed encoding naming rule

--- src/server-ffmpeg/app.ts
+++ src/server-ffmpeg/app.ts
@@ -1,4 +1,4 @@
-import { join, basename } from 'path';
+import { join, basename, parse } from 'path';
 import * as fs from 'fs';
 
 import { ffprobe, ffmpeg, VideoInfo, EncodingStatus } from 'src/ffmpeg';
@@ -10,7 +10,9 @@ async function encodeIfExists() {
   if (queuedList.length > 0) {
     const queued = queuedList[0];
     const path = join('/archive', queued.target);
-    const outpath = path + '.mp4';
+    const parsed = parse(path);
+    const outpath = parsed.dir + '/' + parsed.name + '.mp4';
+    const tmppath = parsed.dir + '/' + parsed.name + '.tmp.mp4';
     const info: VideoInfo = await ffprobe(path);
 
     await ffmpeg.pass1(path, (status: EncodingStatus) => {
@@ -19,14 +21,14 @@ async function encodeIfExists() {
       console.log(new Date().toLocaleString(), basename(queued.target), progress);
     });
 
-    await ffmpeg.pass2(path, outpath, (status: EncodingStatus) => {
+    await ffmpeg.pass2(path, tmppath, (status: EncodingStatus) => {
       const progress = status.frame / info.frame * 50 + 50;
       Encode.updateProgress(queued._id, progress);
       console.log(new Date().toLocaleString(), basename(queued.target), progress);
     });
 
     fs.unlinkSync(path);
-    fs.renameSync(outpath, path);
+    fs.renameSync(tmppath, outpath);
   }
 
   setTimeout(encodeIfExists, 1000);
