1c150793213139b659d40097d9481c075bea5bd4
Hyunsub Kim <embrapers263@gmail.com>
2020-01-18 10:04:17 +0000

    Updated encode presets

--- src/routes/encodes.ts
+++ src/routes/encodes.ts
@@ -48,10 +48,9 @@ router.post('/', (req: Request, res: Response, next: NextFunction) => {
 
 router.get('/presets', (req: Request, res: Response, next: NextFunction) => {
   const presets = {
-    pass1: '-c:v libx264 -b:v 2000k -pass 1 -vf scale=1280:-2 -map_chapters -1 -f mp4 -an -y',
-    pass2: '-c:v libx264 -b:v 2000k -pass 2 -vf scale=1280:-2 -c:a aac -b:a 128k -ac 2 -map_chapters -1 -y',
-    mkv2mp4: '-c:v copy -c:a aac -b:a 128k -ac 2 -map_chapters -1 -y',
-    maxrate: '-c:v libx264 -b:v 2000k -maxrate 2000k -bufsize 4000k -vf scale=1280:-2 -c:a aac -b:a 128k -ac 2 -map_chapters -1 -y',
+    default: '-c:v libx264 -c:a aac -map_chapters -1 -y',
+    audio:   '-c:v copy -c:a aac -map_chapters -1 -y',
+    maxrate: '-c:v libx264 -b:v 2000k -maxrate 2000k -bufsize 4000k -vf scale=1280:-2 -c:a aac -map_chapters -1 -y',
   };
 
   res.status(200);
