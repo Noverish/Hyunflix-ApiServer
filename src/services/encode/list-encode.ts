af39540d3f95eaf14febebdc6b44920dab594e04
Hyunsub Kim <embrapers263@gmail.com>
2020-04-07 14:01:07 +0000

    search by status and order in list-encode

--- src/services/encode/list-encode.ts
+++ src/services/encode/list-encode.ts
@@ -2,17 +2,22 @@ import * as Joi from '@hapi/joi';
 
 import { Encode } from '@src/entity';
 import { EncodeDTO } from '@src/models';
+import { FindManyOptions, IsNull, Not } from 'typeorm';
 
 interface Schema {
   q?: string;
   p: number;
   ps?: number;
+  status?: 'queued' | 'processing' | 'done';
+  order: 'DESC' | 'ASC';
 }
 
 const schema = Joi.object({
   q: Joi.string().optional().empty(''),
   p: Joi.number().optional().default(1),
   ps: Joi.number().optional(),
+  status: Joi.string().optional().allow('queued', 'processing', 'done'),
+  order: Joi.string().optional().allow('DESC', 'ASC').default('DESC'),
 });
 
 export default async function (args: object) {
@@ -21,12 +26,24 @@ export default async function (args: object) {
     throw new Error(`400 - ${error.message}`);
   }
 
-  const { q: query, p: page, ps: pageSize } = value as Schema;
+  const { q: query, p: page, ps: pageSize, status, order } = value as Schema;
 
-  const found: Encode[] = await Encode.find({
-    order: { id: 'DESC' },
+  const options: FindManyOptions<Encode> = {
+    order: { id: order },
     relations: ['before', 'after'],
-  });
+  };
+
+  if (status) {
+    if (status === 'queued') {
+      options.where = { before: IsNull(), after: IsNull() };
+    } else if (status === 'processing') {
+      options.where = { before: Not(IsNull()), after: IsNull() };
+    } else if (status === 'done') {
+      options.where = { before: Not(IsNull()), after: Not(IsNull()) };
+    }
+  }
+
+  const found: Encode[] = await Encode.find(options);
 
   const searched: Encode[] = (query)
     ? found.filter(v => v.inpath.includes(query) || v.outpath.includes(query) || v.options.includes(query))
