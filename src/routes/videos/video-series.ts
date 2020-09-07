30348525acdbfa24e7ceefa579a7e5856593ecc9
Hyunsub Kim <hyunsub@Hyunsubui-MacBookPro.local>
2020-09-07 20:27:46 +0900

    Use authority in video-series

--- src/routes/videos/video-series.ts
+++ src/routes/videos/video-series.ts
@@ -1,17 +1,27 @@
 import { Router, Request, Response, NextFunction } from 'express';
 
 import { VideoSeries, Video } from '@src/entity';
-import { IVideoSeries, IVideo } from '@src/models';
+import { IVideoSeries, IVideo, TokenPayload } from '@src/models';
+import { FindConditions, Raw } from 'typeorm';
+import { TOKEN_PAYLOAD_FIELD } from '@src/config';
 
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  const { authority }: TokenPayload = req[TOKEN_PAYLOAD_FIELD];
+
   (async () => {
     const query: string = req.query['q'] || '';
     const page: number = parseInt(req.query['p'] || '1', 10);
     const pageSize: number = parseInt(req.query['ps'] || '0', 10);
 
-    const tmp: VideoSeries[] = await VideoSeries.$find(undefined, { order: { id: 'DESC' } });
+    const where: FindConditions<Video> = {
+      authority: Raw(`${authority} & VideoSeries.authority`),
+    };
+
+    const tmp: VideoSeries[] = await VideoSeries.$find(where, {
+      order: { id: 'DESC' },
+    });
     const items: IVideoSeries[] = tmp.map(s => s.convert());
 
     const searched = (query)
@@ -31,6 +41,8 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 });
 
 router.get('/:seriesId', (req: Request, res: Response, next: NextFunction) => {
+  const { authority }: TokenPayload = req[TOKEN_PAYLOAD_FIELD];
+
   (async () => {
     const seriesId: number = parseInt(req.params['seriesId'], 10);
 
@@ -41,6 +53,8 @@ router.get('/:seriesId', (req: Request, res: Response, next: NextFunction) => {
       res.json({ msg: 'Not Found' });
       return;
     }
+
+    series.videos = series.videos.filter(v => v.authority === (authority & v.authority));
 
     res.status(200);
     res.json(series.convert());
